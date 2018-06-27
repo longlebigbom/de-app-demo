5941da87e41384de62d37b6681c14566dd47f5c1
Le Xuan Long <long@Les-MacBook-Pro.local>
2018-06-28 00:07:43 +0700

    finish set token

--- src/app/components/_deApp/setToken.js
+++ src/app/components/_deApp/setToken.js
@@ -5,6 +5,8 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 import './deApp.css';
 import { abiArr, whitelistAbi, inputCtList } from './dataConfig';
+import { rejects } from 'assert';
+import { resolve } from 'uri-js';
 
 const web3 = global.web3;
 class setToken extends PureComponent {
@@ -14,6 +16,8 @@ class setToken extends PureComponent {
         this.state = {
             startDate: moment(),
             endDate: moment(),
+            isLoading: false,
+            idTransaction: null,
             isError: true,
             errorMess: null
         };
@@ -40,17 +44,20 @@ class setToken extends PureComponent {
         const { startDate, endDate } = this.state;
         if (startDate >= endDate) {
             this.setState({
-                isError: true
+                isError: true,
+                errorMess: 'End time must be greater than start time'
             });
         } else {
             this.setState({
-                isError: false
+                isError: false,
+                errorMess: null
             });
         }
     }
 
     onSubmit() {
         const { startDate, endDate } = this.state;
+        const errText = 'Some thing went wrong';
 
         if (startDate >= endDate) {
             this.setState({
@@ -63,28 +70,81 @@ class setToken extends PureComponent {
 
         this.setState({
             isError: false,
-            errorMess: null
+            errorMess: null,
+            isLoading: true
         });
 
         const contractAddress = '0xe3e414b735d2af6d24e4d93d00a826f9f08d1743';
         const contract = web3.eth.contract(abiArr).at(contractAddress);
         const tsStart = parseInt(startDate.toDate().getTime() / 1000, 10);
         const tsEnd = parseInt(endDate.toDate().getTime() / 1000, 10);
-        const dataSend = contract.setTimeSale.getData(tsStart, tsEnd);
+        const dataSent = contract.setTimeSale.getData(tsStart, tsEnd);
 
-        web3.eth.getTransactionCount(web3.eth.defaultAccount, (e1, count) => {
-            web3.eth.estimateGas(
-                {
-                    to: contractAddress,
-                    data: dataSend
-                },
-                (e2, count) => {}
-            );
+        web3.eth.getTransactionCount(web3.eth.defaultAccount, (err1, resTrans) => {
+            if (err1) {
+                this.setState({
+                    isError: true,
+                    errorMess: errText
+                });
+            } else {
+                web3.eth.estimateGas(
+                    {
+                        to: contractAddress,
+                        data: dataSent
+                    },
+                    (err2, resEstima) => {
+                        if (err2) {
+                            this.setState({
+                                isError: true,
+                                errorMess: errText
+                            });
+                        } else {
+                            const dataTrans = {
+                                from: web3.eth.defaultAccount,
+                                to: contractAddress,
+                                nonce: web3.toHex(resTrans),
+                                gasPrice: web3.toHex(web3.toWei(0.00000003)),
+                                gasLimit: web3.toHex(resEstima),
+                                value: '0x00',
+                                data: dataSent
+                            };
+                            web3.eth.sendTransaction(dataTrans, (err3, resSendTrans) => {
+                                if (err3) {
+                                    this.setState({
+                                        isError: true,
+                                        errorMess: errText
+                                    });
+                                } else {
+                                    this.intervalId = setInterval(() => {
+                                        web3.eth.getTransactionReceipt(resSendTrans, (err5, value) => {
+                                            if (err5) {
+                                                this.setState({
+                                                    isError: true,
+                                                    errorMess: errText
+                                                });
+                                            } else if (value) {
+                                                this.setState({
+                                                    isError: false,
+                                                    isLoading: false,
+                                                    errorMess: null,
+                                                    idTransaction: value.transactionHash.toString()
+                                                });
+                                                console.log('result: ', value);
+                                                clearInterval(this.intervalId);
+                                            }
+                                        });
+                                    }, 10000);
+                                }
+                            });
+                        }
+                    }
+                );
+            }
         });
     }
 
     render() {
-        const { startDate, endDate, isError, errorMess } = this.state;
+        const { startDate, endDate, isError, errorMess, idTransaction, isLoading, resSendTrans } = this.state;
 
         return (
             <div className="deApp">
@@ -116,11 +176,22 @@ class setToken extends PureComponent {
                         onBlur={this.handleOnBlur}
                     />
                 </div>
+                {errorMess && <div className="err">{errorMess}</div>}
                 <div className="btn-list">
                     <button className={isError ? 'disabled' : ''} onClick={!isError ? this.onSubmit : null}>
                         Set
                     </button>
                 </div>
+                {isLoading && <div className="loading">Loading...</div>}
+                {idTransaction && (
+                    <div className="transId">
+                        <p className="lblSucc">Successed</p>
+                        <p>
+                            <strong>TransactionHash: </strong>
+                            {idTransaction}
+                        </p>
+                    </div>
+                )}
             </div>
         );
     }
